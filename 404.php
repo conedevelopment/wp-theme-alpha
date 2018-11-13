74909cf7e55709570f5f78554cae3ee13d6125e8
Adam Laki <info@adamlaki.com>
2018-11-13 10:55:27 +0100

    Format 404 page

--- 404.php
+++ 404.php
@@ -10,8 +10,23 @@
 get_header();
 ?>
 
-	<?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'pine-alpha' ); ?>
-	<?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'pine-alpha' ); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="page-404">
+					<div class="page-404__inside">
+						<span class="page-404__error-code">404</span>
+
+						<h1 class="page-404__title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'pine-alpha' ); ?></h1>
+
+						<p class="page-404__description"><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'pine-alpha' ); ?></p>
+
+						<a href="<?php echo get_home_url(); ?>" class="btn"><?php esc_html_e( 'Go to Home', 'pine-alpha' ); ?></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 <?php
 get_footer();
