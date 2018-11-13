a2e63cad87037f270ab09e44966f9eb8161b3a9d
Adam Laki <info@adamlaki.com>
2018-11-13 10:06:48 +0100

    Fix meta separator

--- template-parts/post/list-parts/part-meta.php
+++ template-parts/post/list-parts/part-meta.php
@@ -1,7 +1,6 @@
 <?php if ( get_post_format() != 'quote' && get_post_format() != 'aside' ): ?>
     <div class="list-item__meta">
         <?php pine_alpha_posted_on(); ?>
-        <span class="list-item__separator"></span>
         <?php pine_alpha_get_category_list( $post->ID, true ); ?>
     </div>
 <?php endif; ?>
--- template-parts/post/secondary-list-parts/part-meta.php
+++ template-parts/post/secondary-list-parts/part-meta.php
@@ -1,5 +1,4 @@
 <div class="list-item__meta">
     <?php pine_alpha_posted_on(); ?>
-    <span class="list-item__separator"></span>
     <?php pine_alpha_get_category_list( $post->ID, true ); ?>
 </div>
