0a311671f8bdf02ff9ef676c4115cc0fe6bcf66e
Adam Laki <info@adamlaki.com>
2018-12-07 10:09:40 +0100

    Fix comments and slick carousel

--- comments.php
+++ comments.php
@@ -27,54 +27,52 @@ if ( post_password_required() ) {
 		echo 'is-narrow';
 	} ?>
 ">
-	<div class="post-comment__list">	
-		<?php
-			// You can start editing here -- including this comment!
-			if ( have_comments() ) :
-				?>
-				<h3 class="comment-title">
-					<?php
-					$pine_alpha_comment_count = get_comments_number();
-					if ( '1' === $pine_alpha_comment_count ) {
-						printf(
-							/* translators: 1: title. */
-							esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'pine-alpha' ),
-							'<span>' . get_the_title() . '</span>'
-						);
-					} else {
-						printf( // WPCS: XSS OK.
-							/* translators: 1: comment count number, 2: title. */
-							esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $pine_alpha_comment_count, 'comments title', 'pine-alpha' ) ),
-							number_format_i18n( $pine_alpha_comment_count ),
-							'<span>' . get_the_title() . '</span>'
-						);
-					}
-					?>
-				</h3>
-
-				<?php the_comments_navigation(); ?>
-				
-				<ul class="comment-list">
-					<?php
-					wp_list_comments( array(
-						'style'      => 'ul',
-						'short_ping' => true,
-					) );
-					?>
-				</ul>
-
+	
+	<?php if ( have_comments() ) : ?>
+		<div class="post-comment__list">
+			<h3 class="comment-title">
 				<?php
-				the_comments_navigation();
+				$pine_alpha_comment_count = get_comments_number();
+				if ( '1' === $pine_alpha_comment_count ) {
+					printf(
+						/* translators: 1: title. */
+						esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'pine-alpha' ),
+						'<span>' . get_the_title() . '</span>'
+					);
+				} else {
+					printf( // WPCS: XSS OK.
+						/* translators: 1: comment count number, 2: title. */
+						esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $pine_alpha_comment_count, 'comments title', 'pine-alpha' ) ),
+						number_format_i18n( $pine_alpha_comment_count ),
+						'<span>' . get_the_title() . '</span>'
+					);
+				}
+				?>
+			</h3>
 
-				// If comments are closed and there are comments, let's leave a little note, shall we?
-				if ( ! comments_open() ) :
-					?>
-					<?php esc_html_e( 'Comments are closed.', 'pine-alpha' ); ?>
-					<?php
-				endif;
-			endif; // Check for have_comments().
-		?>
-	</div>
+			<?php the_comments_navigation(); ?>
+			
+			<ul class="comment-list">
+				<?php
+				wp_list_comments( array(
+					'style'      => 'ul',
+					'short_ping' => true,
+				) );
+				?>
+			</ul>
+
+			<?php
+			the_comments_navigation();
+
+			// If comments are closed and there are comments, let's leave a little note, shall we?
+			if ( ! comments_open() ) :
+				?>
+				<?php esc_html_e( 'Comments are closed.', 'pine-alpha' ); ?>
+				<?php
+			endif; ?>
+		</div>
+	<?php endif; ?>
+	
 	<div class="post-comment__form">
 		<?php comment_form(); ?>
 	</div>
